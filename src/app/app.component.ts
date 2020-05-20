a555d1793cf89eb1ba29b156d56568c49bccfe5b
Hubert Dzikuć <hubert.dzikuc@gmail.com>
2020-05-20 21:35:09 +0200

    quick reformat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     if (token) {
       this.authService.decodedToken = this.jwtHelper.decodeToken(token);
     }
-    if(user){
+    if (user) {
       this.authService.currentUser = user;
     }
   }
